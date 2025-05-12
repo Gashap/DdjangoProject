0cd3f5db3a4ebb6c427699c5ae3af6ef0db7849e
ShiftLock <ukraina65485@gmail.com>
2025-05-13 00:05:43 +0500

    Revert "Some fix"
    This reverts commit 43d91de75dc5b1bbe6545977ed1034a4bcaae00e.

--- vacancies_website/views.py
+++ vacancies_website/views.py
@@ -1,25 +1,39 @@
 import sqlite3
+
 import requests
 from django.shortcuts import render
+
 from vacancies_website.currency import create_currency_table
 from vacancies_website.utilities import *
 
+
 # conn = sqlite3.connect('mydatabase.db', check_same_thread=False)
 # vacancies_table = 'vacancies'
+#
+# file_name = "C:/Users/eldo3/Downloads/vacancies.csv"
+# vacancies = pd.read_csv(file_name, dtype={'name': str, 'key_skills': str, 'published_at': str})
+# vacancies = vacancies.to_sql(vacancies_table, conn, if_exists='replace', index=False)
+# vac_name = 'java'
+
 
 def home_page(request):
+    # create_currency_table()
     return render(request, 'home_page.html')
 
 
 def demand_page(request):
+    # Demain.get_demain_info()
     return render(request, 'demand_page.html')
 
 
 def geography_page(request):
+    # Gegraphy.get_geograpgy_info()
     return render(request, 'geography_page.html')
 
 
 def skills_page(request):
+    # Skills.get_top_skills_all_time(vacancies)
+    Skills.get_top_skills(vacancies_table, vac_name)
     return render(request, 'skills_page.html')
 
 
